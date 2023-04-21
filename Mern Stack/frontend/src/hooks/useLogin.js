63ef19fdae0da1057d25f8a707db7801e067622c
awwstin <li.jiashun1703@gmail.com>
2023-04-20 21:20:42 -0500

    k

--- Mern Stack/frontend/src/hooks/useLogin.js
+++ Mern Stack/frontend/src/hooks/useLogin.js
@@ -10,11 +10,13 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('${API_BASE_URL}/api/user/login', {
+
+    const response = await fetch(`${API_BASE_URL}/api/user/login`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
     })
+
     const json = await response.json()
 
     if (!response.ok) {
